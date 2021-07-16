158cd27530da802cc485c8144c49283c0d810f39
Tico-hue <lautarotico@gmail.com>
2021-07-16 17:02:06 -0300

    stuff

--- client/my-app/src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import { loginReducer } from "./reducers/LoginReducer";
-
-const reducers = combineReducers({
-  logged: loginReducer,
-});
-
-export default reducers;
--- client/my-app/src/redux/store/index.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { loginReducer } from "../reducers/LoginReducer";
-const store = createStore(loginReducer, ["Use Redux"]);
-
-export default store;
